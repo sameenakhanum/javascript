8c340b8cf7c1c20d9ba785192786b51e7abaeff9
sameena <sameenakhanum007@gmail.com>
2021-07-25 00:35:19 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+//  Data types
+// 1. Primitive data types
+// 2. Refferential data type
+
+
+// 1.Primitive data types
+//  string
+//  Number
+//  Boolean
+//  Undefine
+//  Null
+//  Symbol
+
+// var name = 'sameena';
+// var age = 22;
+// var areyougirl = true;
+// var n = null;
+// var firstName =
+
+
+// console.log(typeof(name));
+// console.log(typeof(age));
+// console.log(typeof(areyougirl));
+// console.log(typeof(n));
+// console.log(typeof(firstName));
+
+
