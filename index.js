1ccbf99229cc5df72fe03bb5e99ab001a01a41f9
sameena <sameenakhanum007@gmail.com>
2021-08-01 09:59:04 +0530

    updated

--- index.js
+++ index.js
@@ -25,3 +25,49 @@
 // console.log(typeof(firstName));
 
 
+
+
+
+// Arrays - collection of data items under a string name 
+
+// var colors = ['black','perple','pink','blue']
+// console.log(colors);
+
+// console.log(colors[3]);
+// console.log(colors.length);
+
+
+// var colors = ['black','perple','pink','blue']
+
+// push - add the element at last
+// colors.push('white');
+
+// pop - remove the element at last
+// colors.pop()
+
+// unshift - add the element at fist
+// colors.unshift('yellow');
+
+// shift - remove the element at first
+// colors.shift();
+
+// splice
+// colors.splice(2,0,'brown')
+
+// console.log(colors);
+
+
+// concat
+
+// var a = [black, white]
+// var b = [pink,perple]
+// var c = a.concat(b);
+// var c =b.concat(a);
+// console.log(c);
+
+// iterating over an array
+
+// var animals = ['cat', 'dog','fox','duck','sheep','deer','lion','fox','hen','camel']
+// for(var i=0; i<= animals.length -1; i++) {
+//     console.log(animals[i]);
+// }
